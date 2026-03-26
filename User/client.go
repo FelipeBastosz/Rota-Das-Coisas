2ed6110cad033e4938917001e4b6f4a3fa7c1487
Felipe Bastos <lipe-ogaroto@outlook.com>
2026-03-26 16:05:54 -0300

    feat: Modularização do menu, adiciona a opção de limpar o terminal e formata as mensagens recebidas do servidor

--- User/client.go
+++ User/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -28,24 +29,25 @@ func main() {
 	go func() {
 		scanner := bufio.NewScanner(conn)
 		for scanner.Scan() {
-			fmt.Printf("\r\033[K%s\nInsira o comando para enviar ao servidor: ", scanner.Text())
+			mensagem := scanner.Text()
+			fmt.Printf("\r\033[K%s\n", mensagem)
+			//Se a mensagem recebida for a de desconectar, ela é interceptada e encerramos o cliente
+			if strings.Contains(mensagem, "se desconectou!") {
+				os.Exit(0)
+			}
+			fmt.Printf("Insira o comando para enviar ao servidor: ")
 		}
-		fmt.Println("\n[ERRO] Servidor foi encerrado.")
+		fmt.Println("\n[ERRO] Conexão com o servidor foi perdida!")
 		os.Exit(0)
 	}()
 
 	//Função para enviar comandos para o servidor
 	scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Println("======= LISTA DE COMANDOS =======")
-	fmt.Println("[1] Receber dados do sensor: 'receber [id]'")
-	fmt.Println("[2] Parar de receber dados do sensor: 'parar'")
-	fmt.Println("[3] Listar os sensores e atuadores disponíveis: 'listar'")
-	fmt.Println("[4] Enviar comando para o atuador: atuar [ID_Atuador] [AÇÃO]")
-	fmt.Println("[5] Desconectar do servidor: 'sair'")
-	fmt.Println("[6] Limpar terminal: 'limpar'")
-	fmt.Println("[7] Ajuda para comandos: 'help'")
+	//Imprime o menu de opções
+	imprimirMenu()
 
+	//Loop para enviar comandos ao servidor
 	for {
 		if !scanner.Scan() {
 			break
@@ -54,12 +56,14 @@ func main() {
 
 		//Verifica logo aqui se o usuário quer limpar o terminal, sem precisar enviar para o servidor tratar
 		if strings.ToLower(strings.TrimSpace(comando)) == "limpar" {
+			//Tenta limpar o terminal no Linux
 			cmd := exec.Command("clear")
 			cmd.Stdout = os.Stdout
 			err := cmd.Run()
 
+			//Se não for linux, ele vai tentar executar o comando do Windows
 			if err != nil {
-				cmd = exec.Command("cmd", "/c", "clear")
+				cmd = exec.Command("cmd", "/c", "cls")
 				cmd.Stdout = os.Stdout
 				cmd.Run()
 			}
@@ -73,4 +77,16 @@ func main() {
 		}
 
 	}
+}
+
+func imprimirMenu() {
+	fmt.Println("======= LISTA DE COMANDOS =======")
+	fmt.Println("[1] Receber dados do sensor: 'receber [id]'")
+	fmt.Println("[2] Parar de receber dados do sensor: 'parar'")
+	fmt.Println("[3] Listar os sensores e atuadores disponíveis: 'listar'")
+	fmt.Println("[4] Enviar comando para o atuador: atuar [ID_Atuador] [AÇÃO]")
+	fmt.Println("[5] Desconectar do servidor: 'sair'")
+	fmt.Println("[6] Limpar terminal: 'limpar'")
+	fmt.Println("[7] Ajuda para comandos: 'help'")
+	fmt.Println("=================================")
 }
